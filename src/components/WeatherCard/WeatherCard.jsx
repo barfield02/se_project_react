82e4a7f3029f52f339511a77470483fda7a74bc5
keia joseph <rahkeiab@gmail.com>
2025-04-21 20:32:22 -0500

    dynamic weatherCard

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,13 +1,33 @@
 import "../WeatherCard/WeatherCard.css";
-import sunny from "../../assets/sunny.png";
+import clear from "../../assets/day/clear.svg";
+import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
 
 function WeatherCard(weatherData) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+
+  let weatherOption;
+  if (filteredOptions.length === 0) {
+    weatherOption = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
+  } else {
+    weatherOption = filteredOptions[0];
+  }
+
   return (
     <section className="weather-card">
       <p className="weather-card__temp">{weatherData.temp} &deg; F</p>
-      <img src={sunny} alt="" className="weather-card__image" />
+      <img
+        src={weatherOption?.url}
+        alt={`Card showing ${weatherOption?.day ? "day" : "night"} time ${
+          weatherOption?.condition
+        } weather`}
+        className="weather-card__image"
+      />
     </section>
   );
 }
-
 export default WeatherCard;
